914950c19b11a8710b33496900b612a6af61452a
Palash Singh Raghuwanshi <singhpalash0@gmail.com>
2023-04-16 11:13:59 +0530

    Search only on pressing enter - temporarily.

--- frontend/pages/search.js
+++ frontend/pages/search.js
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 
 export default function Search() {
-    async function onType(event){
+    async function onEnter(event){
+        if (event.key !== "Enter") {
+            return;
+        }
         await fetch(`http://127.0.0.1:8000/api/search/?q=${event.target.value}`)
         .then(response => response.json())
         .then(data => console.log(data))
@@ -13,7 +16,7 @@ export default function Search() {
         <div className="container mx-auto py-20">
             <div className="block w-5/6 md:w-3/5 mx-auto rounded-2xl shadow-xl">
                 <Image src="/images/magnifying-glass.svg" className="inline mx-1" height={30} width={30} alt="Magnifying glass"></Image>
-                <input type="text" onChange={onType} className="md:w-11/12  h-9 placeholder:text-md outline-none mx-auto bg-inherit" placeholder="Search a movie..."></input>
+                <input type="text" onKeyDown={onEnter} className="md:w-11/12  h-9 placeholder:text-md outline-none mx-auto bg-inherit" placeholder="Search a movie..."></input>
             </div>
         </div>
     )
